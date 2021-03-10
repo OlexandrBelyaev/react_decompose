f178af9cb39dfd70a7c184b2bca07b44bef2c748
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-10 21:03:41 +0200

    Updates

---
+++ precache-manifest.1dc261c8443818ff9326fc24b2ab1b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dd9e92528a1e2c4dcb7ed53ebc387f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7daa2822c00995eedfa",
+    "url": "./static/css/main.6f19a6dc.chunk.css"
+  },
+  {
+    "revision": "8fa8843e8c88d99aef56",
+    "url": "./static/js/2.24585e7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.24585e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7daa2822c00995eedfa",
+    "url": "./static/js/main.724420ef.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
